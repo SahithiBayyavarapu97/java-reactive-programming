2661db5e9caf9c1750c71a44d51b40a6cb155bc4
SahithiBayyavarapu97 <sahithi.bayyavarapu97@gmail.com>
2024-03-07 18:45:00 -0700

    reactive programming flux, mono

--- src/main/java/io/javabrains/reactiveworkshop/ReactiveFluxRead.java
+++ src/main/java/io/javabrains/reactiveworkshop/ReactiveFluxRead.java
@@ -2,17 +2,18 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
-public class Exercise2 {
+public class ReactiveFluxRead {
 
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        // subscribe is like foreach in collections
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
 
-        // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        // Print all users in the xReactiveSources.userFlux stream
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/ReactiveMonoRead.java
+++ src/main/java/io/javabrains/reactiveworkshop/ReactiveMonoRead.java
@@ -2,17 +2,18 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
-public class Exercise4 {
+public class ReactiveMonoRead {
 
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumberMono()
 
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(System.out::println);
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        Integer n = ReactiveSources.intNumberMono().block();
+        System.out.println("n.." + n);
 
         System.out.println("Press a key to end");
         System.in.read();
